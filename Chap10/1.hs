db4a9ac193e59014d8f716bb72aa7d4074c77d7c
Seno <caiodnh@protonmail.com>
2020-11-12 22:19:59 -0330

    fib with scanl

--- Chap10/1.hs
+++ Chap10/1.hs
@@ -1,7 +1,3 @@
-myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr f b [] = b
-myFoldr f b (x:xs) = f x $ myFoldr f b xs
-
 myLength :: [a] -> Int
 myLength = myFoldr (const (+1)) 0 
 
@@ -18,6 +14,19 @@ plusPlus = flip (myFoldr (:))
 
 plusPlus' = flip $ myFoldr (:)
 
+-- 
+
+myFoldr :: (a -> b -> b) -> b -> [a] -> b
+myFoldr f b [] = b
+myFoldr f b (x:xs) = f x $ myFoldr f b xs
+
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
 myFoldl f b [] = b
 myFoldl f b (x:xs) = myFoldl f (f b x) xs
+
+-- 
+
+myScanr :: (a -> b -> b) -> b -> [a] -> [b]
+myScanr f b []     = [b]
+myScanr f b (x:xs) = f x (head old):old 
+  where old = myScanr f b xs 
